3035eda6857a058f6fb17c2dc22b184480c2b23d
mein0819 <mario.munafo28@gmail.com>
2022-05-29 20:17:47 -0700

    adjusted table text color and added readme images

--- scraping.py
+++ scraping.py
@@ -97,7 +97,7 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html(classes="table table-dark table-bordered")
+    return df.to_html(classes="table table-dark table-bordered text-info")
 
 # Function for scraping hi-res hemisphere images
 
